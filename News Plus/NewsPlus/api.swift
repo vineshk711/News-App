185c896153c49bc0bfc7d61cd288ad5e5c73e5ec
Vinesh Kumar <63194027+vineshk711@users.noreply.github.com>
2021-02-04 10:28:28 +0530

    Update api.swift

--- News Plus/NewsPlus/api.swift
+++ News Plus/NewsPlus/api.swift
@@ -2,9 +2,8 @@
 //  api.swift
 //  NewsPlus
 //
-//  Created by Akash on 09/12/20.
-//
 
 import Foundation
-struct API{
-static let key = "64e517c6a3d0410597d761a003f39feb" }
+struct API {
+  static let key = "api_key"
+}
